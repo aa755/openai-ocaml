1ce1f3e43c7305d879765593bea1842460b4a114
Abhishek Anand (optiplex) <abhishek.anand.iitg@gmail.com>
2024-06-18 13:35:29 -0400

    pure string->string function

--- src/test.ml
+++ src/test.ml
@@ -1,5 +1,24 @@
 open Openai
 
+let api_key = Sys.getenv "OPENAI_API_KEY"
+let client = Client.create api_key
+
+let llmquery (s:string) : string =
+  Lwt_main.run (Chat_completion.(
+  send
+    client
+    ~temperature:0.0
+    ~model:"gpt-4o"
+    ~messages:
+      [ { role = `System; content = "You are an expert in logic, formal methods, constructive type theory, dependent type theory, Coq (Gallina, Ltac, Ltac2)" }
+      ; { role = `User; content = s }
+      ]
+    ()))
+
+let pureRun () =
+  print_endline (llmquery "Write an Inductive type denoting the abstract syntax tree of C++ expressions")
+
+(*  
 let run () =
   let api_key = Sys.getenv "OPENAI_API_KEY" in
   let client = Client.create api_key in
@@ -18,5 +37,5 @@ let run () =
            ())
        (Lwt_io.printlf "res: %s")
 ;;
-
-let () = run ()
+*)
+let () = pureRun ()
